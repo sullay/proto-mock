268d2bec7e382c8606d2c102e1513ca0a522df60
sullay <1181518458@qq.com>
2023-04-19 00:07:52 +0800

    test: 新增单元测试

---
+++ test/generateMockData.test.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { generateMockData } = require('proto-mock');
+
+describe('generateMockData', () => {
+  test('returns mocked data for Person message type', async () => {
+    const mockData = await generateMockData(
+      path.resolve(__dirname, './fixtures/person.proto'),
+      'Person'
+    );
+    expect(mockData).toMatchObject({
+      name: expect.any(String),
+      age: expect.any(Number),
+      email: expect.any(Array),
+    });
+  });
+
+  test('returns mocked data with specified maxRepeatedLength', async () => {
+    const mockData = await generateMockData(
+      path.resolve(__dirname, './fixtures/person.proto'),
+      'Person',
+      { maxRepeatedLength: 5 }
+    );
+    expect(mockData.email.length).toBeLessThanOrEqual(5);
+  });
+
+  test('returns mocked data with specified keepCase option', async () => {
+    const mockData = await generateMockData(
+      path.resolve(__dirname, './fixtures/person.proto'),
+      'Person',
+      { keepCase: true }
+    );
+    expect(mockData).toMatchObject({
+      name: expect.any(String),
+      age: expect.any(Number),
+      email: expect.any(Array),
+      email_address: expect.any(String),
+    });
+  });
+
+  test('throws an error when invalid message type is provided', async () => {
+    await expect(
+      generateMockData(path.resolve(__dirname, './fixtures/person.proto'), 'InvalidMessageType')
+    ).rejects.toThrow('no such type: InvalidMessageType');
+  });
+});
